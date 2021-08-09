2dfec3eb948f4ac311bcd5ef475c6206bd1e7c01
tqm-dev <queen.jewels.dev@gmail.com>
2021-08-09 12:33:18 +0900

    Divided shader code into shader.*

---
+++ src/shaders.c
@@ -0,0 +1,216 @@
+/*
+ * Copyright (c) 2021 Takuma Hayashi
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library in the file COPYING;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "shaders.h"
+#include <string.h>
+#include <stdio.h>
+
+const char* vgShaderVertexPipeline = R"glsl(
+    #version 330
+    
+/*** Input *******************/
+    in vec2 pos;
+    in vec2 textureUV;
+    uniform mat4 modelView;
+    uniform mat4 projection;
+
+/*** Output ******************/
+    out vec2 varying_uv;
+
+/*** Main thread  **************************************************/
+    void main() {
+
+        /* Stage 3: Transformation */
+        gl_Position = projection * modelView * vec4(pos, 0, 1);
+        varying_uv = textureUV;
+
+    }
+)glsl";
+
+const char* vgShaderFragmentPipeline = R"glsl(
+
+    #version 330
+
+/*** Enum constans ************************************/
+
+    #define PAINT_TYPE_COLOR			0x1B00
+    #define PAINT_TYPE_LINEAR_GRADIENT	0x1B01
+    #define PAINT_TYPE_RADIAL_GRADIENT	0x1B02
+    #define PAINT_TYPE_PATTERN			0x1B03
+
+    #define DRAW_IMAGE_NORMAL 			0x1F00
+    #define DRAW_IMAGE_MULTIPLY 		0x1F01
+
+    #define DRAW_MODE_PATH				0
+    #define DRAW_MODE_IMAGE				1
+
+/*** Interpolated *************************************/
+
+    in vec2 varying_uv;
+
+/*** Input ********************************************/
+
+    // Basic rendering Mode
+    uniform int drawMode;
+    // Image
+    uniform sampler2D imageSampler;
+    uniform int imageMode;
+    // Paint
+    uniform int paintType;
+    uniform vec4 paintColor;
+    uniform vec2 paintParams[3];
+    uniform mat3 surfaceToPaintMatrix;
+    // Gradient
+    uniform sampler1D rampSampler;
+    // Pattern
+    uniform sampler2D patternSampler;
+    // Color transform
+    uniform vec4 scaleFactorBias[2];
+
+/*** Output *******************************************/
+
+    out vec4 fragColor;
+
+/*** Functions ****************************************/
+
+    // 9.3.1 Linear Gradients
+    float linearGradient(vec2 fragCoord, vec2 p0, vec2 p1){
+
+        float x  = fragCoord.x;
+        float y  = fragCoord.y;
+        float x0 = p0.x;
+        float y0 = p0.y;
+        float x1 = p1.x;
+        float y1 = p1.y;
+        float dx = x1 - x0;
+        float dy = y1 - y0;
+    
+        return
+            ( dx * (x - x0) + dy * (y - y0) )
+         /  ( dx*dx + dy*dy );
+    }
+
+    // 9.3.2 Radial Gradients
+    float radialGradient(vec2 fragCoord, vec2 centerCoord, vec2 focalCoord, float r){
+
+        float x   = fragCoord.x;
+        float y   = fragCoord.y;
+        float cx  = centerCoord.x;
+        float cy  = centerCoord.y;
+        float fx  = focalCoord.x;
+        float fy  = focalCoord.y;
+        float dx  = x - fx;
+        float dy  = y - fy;
+        float dfx = fx - cx;
+        float dfy = fy - cy;
+    
+        return
+            ( (dx * dfx + dy * dfy) + sqrt(r*r*(dx*dx + dy*dy) - pow(dx*dfy - dy*dfx, 2.0)) )
+         /  ( r*r - (dfx*dfx + dfy*dfy) );
+    }
+
+    vec2 backToPaintSpace(vec4 fragCoord){
+
+        vec3 paintCoord = surfaceToPaintMatrix * vec3(fragCoord.xy, 1.0);
+
+        return paintCoord.xy;
+    }
+
+/*** Main thread  *************************************/
+
+    void main()
+    {
+        vec4 col;
+
+        /* Stage 6: Paint Generation */
+        switch(paintType){
+        case PAINT_TYPE_LINEAR_GRADIENT:
+            {
+                vec2  x0 = paintParams[0];
+                vec2  x1 = paintParams[1];
+                float factor = linearGradient(backToPaintSpace(gl_FragCoord), x0, x1);
+                col = texture(rampSampler, factor);
+            }
+            break;
+        case PAINT_TYPE_RADIAL_GRADIENT:
+            {
+                vec2  center = paintParams[0];
+                vec2  focal  = paintParams[1];
+                float radius = paintParams[2].x;
+                float factor = radialGradient(
+                                   backToPaintSpace(gl_FragCoord),
+                                   center,
+                                   focal,
+                                   radius);
+                col = texture(rampSampler, factor);
+            }
+            break;
+        case PAINT_TYPE_PATTERN:
+            {
+                vec2  paintCoord = backToPaintSpace(gl_FragCoord);
+                float width  = paintParams[0].x;
+                float height = paintParams[0].y;
+                vec2  texCoord = vec2(paintCoord.x / width, paintCoord.y / height);
+                col = texture(patternSampler, texCoord);
+            }
+            break;
+        default:
+        case PAINT_TYPE_COLOR:
+            col = paintColor;
+            break;
+        }
+
+        /* Stage 7: Image Interpolation */
+        if(drawMode == DRAW_MODE_IMAGE) {
+            col = texture(imageSampler, varying_uv)
+                      * (imageMode == DRAW_IMAGE_MULTIPLY ? col : vec4(1.0, 1.0, 1.0, 1.0));
+        } 
+
+        /* Stage 8: Color Transformation, Blending, and Antialiasing */
+        fragColor = col * scaleFactorBias[0] + scaleFactorBias[1] ;
+    }
+)glsl";
+
+const char* vgShaderVertexColorRamp = R"glsl(
+    #version 330
+    
+    in  vec2 step;
+    in  vec4 stepColor;
+    out vec4 interpolateColor;
+
+    void main()
+    {
+        gl_Position = vec4(step.xy, 0, 1);
+        interpolateColor = stepColor;
+    }
+)glsl";
+
+const char* vgShaderFragmentColorRamp = R"glsl(
+    #version 330
+
+    in  vec4 interpolateColor;
+    out vec4 fragColor;
+
+    void main()
+    {
+        fragColor = interpolateColor;
+    }
+)glsl";
+
---
+++ src/shaders.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 Takuma Hayashi
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library in the file COPYING;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef __SHADERS_H
+#define __SHADERS_H
+
+extern const char* vgShaderVertexPipeline;
+extern const char* vgShaderFragmentPipeline;
+extern const char* vgShaderVertexColorRamp;
+extern const char* vgShaderFragmentColorRamp;
+
+#endif /* __SHADERS_H */
